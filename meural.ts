8d9557dccba40b6bc9a2f71e90c16a123bdb3dfa
Evan Madow <evan@evanm.com>
2022-06-18 07:46:08 -0700

    temp fix to resolve issue running within cron

--- meural.ts
+++ meural.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import FormData from 'form-data';
 import fs from 'fs';
+import 'url-search-params-polyfill'; // shouldn't need this but will debug later
 
 interface MeuralItem {
   id: number;
